a4a90da90afa646bf4ec074ec9541db9f41530fb
Nihronick <Nihronick@users.noreply.github.com>
2026-05-11 00:24:36 +0000

    deploy: bd6bcb1d0ea36eb0108ff3c3c22719af02e10eb7

---
+++ assets/card-C82XSIkv.js
@@ -0,0 +1,7 @@
+import{c as s,j as t,a as d}from"./index-ck6NWoxs.js";/**
+ * @license lucide-react v1.14.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const e=[["path",{d:"m9 18 6-6-6-6",key:"mthhwq"}]],i=s("chevron-right",e);function c({className:a,size:r="default",...o}){return t.jsx("div",{"data-slot":"card","data-size":r,className:d("group/card flex flex-col gap-4 overflow-hidden rounded-xl bg-card py-4 text-sm text-card-foreground transition-colors ring-1 ring-foreground/10 dark:ring-foreground/5 dark:bg-card/95 has-data-[slot=card-footer]:pb-0 has-[>img:first-child]:pt-0 data-[size=sm]:gap-3 data-[size=sm]:py-3 data-[size=sm]:has-data-[slot=card-footer]:pb-0 *:[img:first-child]:rounded-t-xl *:[img:last-child]:rounded-b-xl",a),...o})}function l({className:a,...r}){return t.jsx("div",{"data-slot":"card-content",className:d("px-4 group-data-[size=sm]/card:px-3",a),...r})}export{c as C,l as a,i as b};
+//# sourceMappingURL=card-C82XSIkv.js.map
