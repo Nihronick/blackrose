84b250ec97ccd973e0b174c5732922169c6f4da7
Nihronick <Nihronick@users.noreply.github.com>
2026-08-07 00:01:08 +0000

    deploy: 3c5355449770917daf737b442211308a7b4cc3a0

---
+++ assets/useSuspenseQuery-CltIRSra.js
@@ -0,0 +1,2 @@
+import{Z as r,$ as u,a0 as s}from"./index-BYDBWpS_.js";function t(e,a){return r({...e,enabled:!0,suspense:!0,throwOnError:s,placeholderData:void 0},u)}export{t as u};
+//# sourceMappingURL=useSuspenseQuery-CltIRSra.js.map
--- assets/useSuspenseQuery-DV6F1GrD.js
+++
@@ -1,2 +0,0 @@
-import{Z as r,$ as u,a0 as s}from"./index-DxbOwZJ0.js";function t(e,a){return r({...e,enabled:!0,suspense:!0,throwOnError:s,placeholderData:void 0},u)}export{t as u};
-//# sourceMappingURL=useSuspenseQuery-DV6F1GrD.js.map
