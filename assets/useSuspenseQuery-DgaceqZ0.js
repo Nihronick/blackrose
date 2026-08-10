1f9ba7d39d27a228bec94e582cbf571e629546db
Nihronick <Nihronick@users.noreply.github.com>
2026-08-10 02:56:25 +0000

    deploy: 65018b9d97b844075b4051345a043bb0256f039e

---
+++ assets/useSuspenseQuery-DgaceqZ0.js
@@ -0,0 +1,2 @@
+import{Z as r,$ as u,a0 as s}from"./index-ZlWVCqVS.js";function t(e,a){return r({...e,enabled:!0,suspense:!0,throwOnError:s,placeholderData:void 0},u)}export{t as u};
+//# sourceMappingURL=useSuspenseQuery-DgaceqZ0.js.map
