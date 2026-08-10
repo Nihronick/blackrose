4534a24e5a6df9bc1fbdb68498c0fbeca9b8c233
Nihronick <Nihronick@users.noreply.github.com>
2026-08-10 02:48:24 +0000

    deploy: d51b18d3b7371d9c7aff1dcd8b20c08181079107

---
+++ assets/useSuspenseQuery-DOwI2hVH.js
@@ -0,0 +1,2 @@
+import{Z as r,$ as u,a0 as s}from"./index-DVBYWoMj.js";function t(e,a){return r({...e,enabled:!0,suspense:!0,throwOnError:s,placeholderData:void 0},u)}export{t as u};
+//# sourceMappingURL=useSuspenseQuery-DOwI2hVH.js.map
