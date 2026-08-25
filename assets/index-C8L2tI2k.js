f4074b26db2dc91b98714c3ab1fa1b4e4d374667
Nihronick <Nihronick@users.noreply.github.com>
2026-08-25 08:34:00 +0000

    deploy: 5de919cb3673a4c8292a07f7ea2c9e3b65249931

---
+++ assets/index-C8L2tI2k.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-D2O1I8-N.js","assets/index-BD_s8qfC.js"])))=>i.map(i=>d[i]);
+import{_ as p}from"./index-BgSzU-qP.js";import{r as o}from"./index-BD_s8qfC.js";import"./vendor-react-TqjFfbtS.js";import"./vendor-tanstack-Ce-S5pTm.js";import"./vendor-charts-DHreujVO.js";import"./vendor-motion-DDRvvPT_.js";const s=o("App",{web:()=>p(()=>import("./web-D2O1I8-N.js"),__vite__mapDeps([0,1])).then(r=>new r.AppWeb)});export{s as App};
